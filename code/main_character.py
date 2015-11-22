32d116b33ff7a11a5d0f86aa6358df418e2535ff
900903ksh <900903ksh@naver.com>
2015-11-22 23:30:21 +0900

    Heal 스킬을 추가하고 혼령 수치에 따라 스킬과 유닛을 소환할 수 있는 기능을 구현

--- code/main_character.py
+++ code/main_character.py
@@ -7,6 +7,14 @@ mc_data_file = open('data/main_character_data.txt', 'r')
 mc_data = json.load(mc_data_file)
 mc_data_file.close()
 
+friend_data_file = open('data/friend_data.txt', 'r')
+friend_data = json.load(friend_data_file)
+friend_data_file.close()
+
+skill_data_file = open('data/skill_data.txt', 'r')
+skill_data = json.load(skill_data_file)
+skill_data_file.close()
+
 stage_data_file = open('data/stage_data.txt', 'r')
 stage_data = json.load(stage_data_file)
 stage_data_file.close()
@@ -25,17 +33,18 @@ class MainCharacter:
     RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
     STAND_RIGHT, STAND_LEFT, MOVE_RIGHT, MOVE_LEFT = 'STAND_RIGHT', 'STAND_LEFT', 'MOVE_RIGHT', 'MOVE_LEFT'
-    SKILL1, SKILL2, ATTACK, SUMMON, ABSORB = 'SKILL1', 'SKILL2', 'ATTACK', 'SUMMON', 'ABSORB'
+    SKILL1, SKILL2, ATTACK, SUMMON, HEAL = 'SKILL1', 'SKILL2', 'ATTACK', 'SUMMON', 'HEAL'
     DIE_RIGHT, DIE_LEFT, HIT_RIGHT, HIT_LEFT = 'DIE_RIGHT', 'DIE_LEFT', 'HIT_RIGHT', 'HIT_LEFT'
 
     def __init__(self):
         self.state = self.STAND_RIGHT
         self.hp = mc_data['hp']
         self.damage = mc_data['damage']
+        self.spirit = mc_data['spirit']
         for i in image.imageList:
-            if i.name == 'Lich':
+            if i.name == 'Eregos':
                 self.image = i.image
-        self.x, self.y = 200, stage_data['stage2']['bottom'] + mc_data['pivotY']
+        self.x, self.y = 200, stage_data['stage1']['bottom'] + mc_data['pivotY']
 
         self.game_time = 0
 
@@ -50,31 +59,22 @@ class MainCharacter:
         self.right_key_pressed = False
         self.summon_name = False
         self.summon_check = False
-
-        self.skill = None
         self.skill_check = False
 
-    def update(self, frame_time, friendList):
+    def update(self, frame_time, friendList, skillList):
         self.change_state()
         self.total_frame += MainCharacter.FRAMES_PER_ACTION * MainCharacter.ACTION_PER_TIME * frame_time
         self.frame = int(self.total_frame) % self.state_frame
         self.state_frame = mc_data[self.state]['frame']
-        self.handle_state[self.state](self, frame_time, friendList)
-
-        if self.skill != None:
-            if self.skill.update(frame_time) != False:
-                self.skill_check = True
-            else:
-                self.skill_check = False
-
-
+        self.handle_state[self.state](self, frame_time, friendList, skillList)
+        if self.spirit < 0: self.spirit = 0
 
     def draw(self):
         self.image.clip_draw(self.frame * mc_data[self.state]['left'], mc_data[self.state]['bottom'],
                              mc_data[self.state]['width'], mc_data[self.state]['height'],
                              self.x + mc_data[self.state]['plusX'], self.y + mc_data[self.state]['plusY'])
-        if self.skill_check == True:
-            self.skill.draw()
+
+        # draw_rectangle(*self.get_bb())
 
     def handle_events(self,event):
         if self.key_lock == False:
@@ -102,27 +102,38 @@ class MainCharacter:
                 elif event.key == SDLK_4: self.summon_name = 'SkelSpearknight'
                 elif event.key == SDLK_5: self.summon_name = 'Wraith'
                 elif event.key == SDLK_6: self.summon_name = 'MuscleStone'
-                self.summon_check = True
-                self.state = self.SUMMON
-                self.key_lock_func()
+
+                if self.spirit >= friend_data[self.summon_name]['need_spirit']:
+                    self.spirit -= friend_data[self.summon_name]['need_spirit']
+                    print(self.spirit)
+                    self.summon_check = True
+                    self.state = self.SUMMON
+                    self.key_lock_func()
 
             elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_a):
                 self.state = self.ATTACK
                 self.key_lock_func()
 
-            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_s):
-                self.skill = skill.Skill1(self.x)
-                self.state = self.SKILL1
-                self.key_lock_func()
+            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_q):
+                if self.spirit >= skill_data['skill1']['need_spirit']:
+                    self.spirit -= skill_data['skill1']['need_spirit']
+                    self.skill_check = True
+                    self.state = self.SKILL1
+                    self.key_lock_func()
 
-            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_d):
-                self.skill = skill.Skill2(self.x)
-                self.state = self.SKILL2
-                self.key_lock_func()
+            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_w):
+                if self.spirit >= skill_data['skill2']['need_spirit']:
+                    self.spirit -= skill_data['skill2']['need_spirit']
+                    self.skill_check = True
+                    self.state = self.SKILL2
+                    self.key_lock_func()
 
-            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_z):
-                self.state = self.ABSORB
-                self.key_lock_func()
+            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_e):
+                if self.spirit >= skill_data['heal']['need_spirit']:
+                    self.spirit -= skill_data['heal']['need_spirit']
+                    self.skill_check = True
+                    self.state = self.HEAL
+                    self.key_lock_func()
 
         elif self.key_lock == True:
             if (event.type, event.key) == (SDL_KEYDOWN, SDLK_RIGHT):
@@ -134,31 +145,50 @@ class MainCharacter:
             elif (event.type, event.key) == (SDL_KEYUP, SDLK_LEFT):
                 self.left_key_pressed = False
 
-    def handle_stand_right(self, frame_time, friendList):
+    def handle_stand_right(self, frame_time, friendList, skillList):
         pass
 
-    def handle_stand_left(self, frame_time, friendList):
+    def handle_stand_left(self, frame_time, friendList, skillList):
         pass
 
-    def handle_move_right(self, frame_time, friendList):
+    def handle_move_right(self, frame_time, friendList, skillList):
         self.x += self.RUN_SPEED_PPS * frame_time
 
-    def handle_move_left(self, frame_time, friendList):
+    def handle_move_left(self, frame_time, friendList, skillList):
         self.x -= self.RUN_SPEED_PPS * frame_time
 
-    def handle_skill1(self, frame_time, friendList):
+    def handle_skill1(self, frame_time, friendList, skillList):
+        if self.skill_check == True:
+            Skill = skill.Skill1(self.x, stage_data['stage1']['bottom'])
+            skillList.append(Skill)
+            self.skill_check = False
         if self.total_frame > self.state_frame:
             self.next_state()
 
-    def handle_skill2(self, frame_time, friendList):
+    def handle_skill2(self, frame_time, friendList, skillList):
+        if self.skill_check == True:
+            Skill = skill.Skill2(self.x, stage_data['stage1']['bottom'])
+            skillList.append(Skill)
+            self.skill_check = False
         if self.total_frame > self.state_frame:
             self.next_state()
 
-    def handle_attack(self, frame_time, friendList):
+    def handle_heal(self, frame_time, friendList, skillList):
+        if self.skill_check == True:
+            Skill = skill.Heal(self.x, self.y)
+            skillList.append(Skill)
+            self.hp += Skill.heal_amount
+            if self.hp > mc_data['hp']:
+                self.hp = mc_data['hp']
+            self.skill_check = False
         if self.total_frame > self.state_frame:
             self.next_state()
 
-    def handle_summon(self, frame_time, friendList):
+    def handle_attack(self, frame_time, friendList, skillList):
+        if self.total_frame > self.state_frame:
+            self.next_state()
+
+    def handle_summon(self, frame_time, friendList, skillList):
         if self.summon_check == True:
             Friend = friend.Friend(self.summon_name, self.x + 150) ## self.x + 200 소환 위치
             friendList.append(Friend)
@@ -166,23 +196,22 @@ class MainCharacter:
         if self.total_frame > self.state_frame:
             self.next_state()
 
-    def handle_absorb(self, frame_time, friendList):
-        if self.total_frame > self.state_frame:
-            self.next_state()
-
-    def handle_die_right(self, frame_time, friendList):
+    def handle_die_right(self, frame_time, friendList, skillList):
         pass
 
-    def handle_die_left(self, frame_time, friendList):
+    def handle_die_left(self, frame_time, friendList, skillList):
         pass
 
-    def handle_hit_right(self, frame_time, friendList):
+    def handle_hit_right(self, frame_time, friendList, skillList):
         self.next_state()
         pass
 
-    def handle_hit_left(self, frame_time, friendList):
+    def handle_hit_left(self, frame_time, friendList, skillList):
         self.next_state()
         pass
+
+    def absorb_spirit(self):
+        self.spirit += 10
 
     handle_state = {
         STAND_RIGHT : handle_stand_right,
@@ -191,9 +220,9 @@ class MainCharacter:
         MOVE_LEFT   : handle_move_left,
         SKILL1      : handle_skill1,
         SKILL2      : handle_skill2,
+        HEAL        : handle_heal,
         ATTACK      : handle_attack,
         SUMMON      : handle_summon,
-        ABSORB      : handle_absorb,
         DIE_RIGHT   : handle_die_right,
         DIE_LEFT    : handle_die_left,
         HIT_RIGHT   : handle_hit_right,
@@ -207,6 +236,10 @@ class MainCharacter:
             self.state_frame = mc_data[self.state]['frame']
             self.total_frame = 0.0
             self.game_time = 0.0
+
+    def get_bb(self):
+        return self.x - mc_data['bb_left'], self.y - mc_data['bb_bottom'],\
+                self.x + mc_data['bb_right'], self.y + mc_data['bb_height']
 
     def key_lock_func(self):
         self.key_lock = True
--- code/skill.py
+++ code/skill.py
@@ -11,33 +11,69 @@ class Skill1:
     ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
     FRAMES_PER_ACTION = 6
 
-    def __init__(self, xpos):
+    SUMMON, ATTACK, DIE = 0, 1, 2
+
+    def __init__(self, xpos, ypos):
         if Skill1.image == None:
             Skill1.image = load_image(skill_data['skill1']['image'])
         self.damage = skill_data['skill1']['damage']
-        self.x_frame = 0
-        self.y_frame = 0
+        self.frame = 0
+        self.state = self.SUMMON
         self.total_frame = 0
         self.skill_frame = skill_data['skill1']['draw']['frame']
-        self.x = xpos
+        self.x = skill_data['skill1']['draw']['xpos'] + xpos
+        self.y = skill_data['skill1']['draw']['ypos'] + ypos
+        self.attack_frame = skill_data['skill1']['attack_frame']
+        self.attack_check = True
 
     def update(self, frame_time):
         self.total_frame += Skill1.FRAMES_PER_ACTION * Skill1.ACTION_PER_TIME * frame_time
-        self.x_frame = int(self.total_frame) % self.skill_frame
+        self.frame = int(self.total_frame) % self.skill_frame
         if self.total_frame >= self.skill_frame:
             self.total_frame = 0
-            self.y_frame += 1
-        if self.y_frame == 2:
+            self.state += self.ATTACK
+        if self.state == self.DIE:
             return False
 
 
     def draw(self):
-        self.image.clip_draw(self.x_frame * skill_data['skill1']['draw']['left'],
-                             self.y_frame * skill_data['skill1']['draw']['bottom'],
-                             skill_data['skill1']['draw']['width'], skill_data['skill1']['draw']['height'],
-                             skill_data['skill1']['draw']['xpos']+self.x, skill_data['skill1']['draw']['ypos'],
+        self.image.clip_draw(self.frame * skill_data['skill1']['draw']['left'],
+                             self.state * skill_data['skill1']['draw']['bottom'],
+                             skill_data['skill1']['draw']['width'], skill_data['skill1']['draw']['height'], self.x, self.y,
                              skill_data['skill1']['draw']['xsize'], skill_data['skill1']['draw']['ysize'])
+        # self.draw_bb()
 
+    def draw_bb(self):
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self):
+        return self.x - skill_data['skill1']['attack_range_left'],\
+                self.y - skill_data['skill1']['attack_range_bottom'],\
+                self.x + skill_data['skill1']['attack_range_right'],\
+                self.y + skill_data['skill1']['attack_range_height']
+
+    def collide(self, target_bb):
+        left_self, bottom_self, right_self, top_self = self.get_bb()
+        left_target, bottom_target, right_target, top_target = target_bb
+
+        if left_self > right_target : return False
+        if right_self < left_target : return False
+        if top_self < bottom_target : return False
+        if bottom_self > top_target : return False
+
+        return True
+
+    def collide_check_func(self, targetList1, targetList2):
+        if self.attack_check == True:
+            if self.state == self.ATTACK:
+                if self.total_frame >= self.attack_frame:
+                    for target in targetList1:
+                        if self.collide(target.get_hit_bb()) == True:
+                            target.skill_hit(self.damage)
+                    for target in targetList2:
+                        if self.collide(target.get_hit_bb()) == True:
+                            target.skill_hit(self.damage)
+                    self.attack_check = False
 
 class Skill2:
     image = None
@@ -48,7 +84,7 @@ class Skill2:
     ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
     FRAMES_PER_ACTION = 6
 
-    def __init__(self, xpos):
+    def __init__(self, xpos, ypos):
         if Skill2.image == None:
             Skill2.image = load_image(skill_data['skill2']['image'])
         self.damage = skill_data['skill2']['damage']
@@ -56,7 +92,10 @@ class Skill2:
         self.total_frame = 0
         self.state = self.SUMMON
         self.skill_frame = skill_data['skill2'][self.state]['frame']
-        self.x = xpos
+        self.x = skill_data['skill2'][self.state]['xpos'] + xpos
+        self.y = ypos
+        self.attack_frame = skill_data['skill2']['attack_frame']
+        self.attack_check = True
 
     def update(self, frame_time):
         self.total_frame += Skill2.FRAMES_PER_ACTION * Skill2.ACTION_PER_TIME * frame_time
@@ -76,5 +115,70 @@ class Skill2:
     def draw(self):
         self.image.clip_draw(self.frame * skill_data['skill2'][self.state]['left'], skill_data['skill2'][self.state]['bottom'],
                              skill_data['skill2'][self.state]['width'], skill_data['skill2'][self.state]['height'],
-                             skill_data['skill2'][self.state]['xpos']+self.x, skill_data['skill2'][self.state]['ypos'],
+                             self.x, self.y + skill_data['skill2'][self.state]['ypos'],
                              skill_data['skill2'][self.state]['xsize'], skill_data['skill2'][self.state]['ysize'])
+        # self.draw_bb()
+
+    def draw_bb(self):
+        draw_rectangle(*self.get_bb())
+
+    def get_bb(self):
+        return self.x - skill_data['skill2']['attack_range_left'],\
+                self.y - skill_data['skill2']['attack_range_bottom'],\
+                self.x + skill_data['skill2']['attack_range_right'],\
+                self.y + skill_data['skill2']['attack_range_height']
+
+    def collide(self, target_bb):
+        left_self, bottom_self, right_self, top_self = self.get_bb()
+        left_target, bottom_target, right_target, top_target = target_bb
+
+        if left_self > right_target : return False
+        if right_self < left_target : return False
+        if top_self < bottom_target : return False
+        if bottom_self > top_target : return False
+
+        return True
+
+    def collide_check_func(self, targetList1, targetList2):
+        if self.attack_check == True:
+            if self.state == self.ATTACK:
+                if self.total_frame >= self.attack_frame:
+                    for target in targetList1:
+                        if self.collide(target.get_hit_bb()) == True:
+                            target.skill_hit(self.damage)
+                    for target in targetList2:
+                        if self.collide(target.get_hit_bb()) == True:
+                            target.skill_hit(self.damage)
+                    self.attack_check = False
+
+
+class Heal:
+    image = None
+
+    TIME_PER_ACTION = 0.5
+    ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+    FRAMES_PER_ACTION = 6
+
+    def __init__(self, xpos, ypos):
+        if Heal.image == None:
+            Heal.image = load_image(skill_data['heal']['image'])
+        self.heal_amount = skill_data['heal']['heal_amount']
+        self.frame = 0
+        self.total_frame = 0
+        self.skill_frame = skill_data['heal']['draw']['frame']
+        self.x = xpos
+        self.y = ypos + skill_data['heal']['draw']['ypos']
+
+    def update(self, frame_time):
+        self.total_frame += Skill1.FRAMES_PER_ACTION * Skill1.ACTION_PER_TIME * frame_time
+        self.frame = int(self.total_frame) % self.skill_frame
+        if self.total_frame >= skill_data['heal']['limit_frame']:
+            return False
+
+    def draw(self):
+        self.image.clip_draw(self.frame * skill_data['heal']['draw']['left'], skill_data['heal']['draw']['bottom'],
+                             skill_data['heal']['draw']['width'], skill_data['heal']['draw']['height'], self.x, self.y,
+                             skill_data['heal']['draw']['xsize'], skill_data['heal']['draw']['ysize'])
+
+    def collide_check_func(self, friendList, monsterList):
+        pass
